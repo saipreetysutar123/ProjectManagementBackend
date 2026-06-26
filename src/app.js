06a6787902ad0a30ec7a16d510949b8fb24b5365
saipreety sutar <saipreetysutar2315@gmail.com>
2026-06-26 15:14:09 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from "cookie-parser";
+import healthCheckRouter from "./routes/routes.js" 
+import authRouter from "./routes/authRoutes.js"
+
+const app = express();
+
+// basic configuration
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+app.use(cookieParser());
+
+//cors configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+    }),
+);
+
+// import the routes
+app.use("/api/v1/healthcheck", healthCheckRouter)
+app.use("/api/v1/auth", authRouter)
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+export default app;
---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+import { User } from "../models/userModel.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiError } from "../utils/api-error.js";
+import jwt from "jsonwebtoken"
+
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+    console.log("Cookies:", req.cookies);
+console.log("Authorization:", req.header("Authorization"));
+
+    const token = req.cookies?.accessToken || req.header("Authorization")?.
+        replace("Bearer ", "")
+console.log("Token:", token);
+    if (!token) {
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+        const user = await User.findById(decodedToken?._id).select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry");
+
+        if (!user) {
+            throw new ApiError(401, "Invalid access token")
+        }
+        req.user = user
+        next();
+    } catch (error) {
+        throw new ApiError(401, "Invalid access token")
+    }
+})
