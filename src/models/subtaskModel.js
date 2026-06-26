d20008c5f83904222ddddb1a016897f9c10440cd
saipreety sutar <saipreetysutar2315@gmail.com>
2026-06-26 17:45:26 +0530

    Models updated

---
+++ src/models/subtaskModel.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const subTaskSchema = new Schema({
+    title: {
+        type: String,
+        required: true, 
+        trim: true
+    },
+    task: {
+        type: Schema.Types.ObjectId,
+        ref: "Task",
+        default: false
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, {timestamps: true})
+
+export const Subtask = mongoose.model("Subtask", subTaskSchema)
